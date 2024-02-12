b929d4228c51a6e0e745304452c845dafab9f4e8
mohamed45679 <159211588+mohamed45679@users.noreply.github.com>
2024-02-12 20:31:56 +0200

    Update m.py

--- m.py
+++ m.py
@@ -1,7 +1,7 @@
 import pandas as pd 
 import streamlit as st
 st.title("cars price prediction:")
-from scikit-learn.ensemble import GradientBoostingRegressor 
+from sklearn.ensemble import GradientBoostingRegressor 
 model=GradientBoostingRegressor()
 data=pd.read_csv(r"carrr.csv")
 x=data.drop(['price'],axis=1)
